71c18515ceb8027a3d91930c8839306e73d46d28
Prajakta <prajaktabhelawe12@gmail.com>
2026-05-16 10:57:42 +0530

    expense-management-system

---
+++ src/main/java/com/company/expense_management_service/ExpenseManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.expense_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseManagementServiceApplication.class, args);
+	}
+
+}
